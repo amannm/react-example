5c19b99b4340ddb87096d8113eb930fb4093e3e7
amannmalik <amannmalik@gmail.com>
2018-03-04 08:52:03 -0600

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,108 @@
+const E = React.createElement;
+
+class AuthenticationPane extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        const logo = E('h1', null, 'Example App');
+        const leftPane = E('div', null, logo);
+
+        const form = E(AuthenticationForm, null);
+        const rightPane = E('div', null, form);
+
+        return E('div', {className: 'AuthenticationPane'},
+            leftPane,
+            rightPane
+        )
+    }
+}
+
+class AuthenticationForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {email: '', password: ''};
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSignUp = this.handleSignUp.bind(this);
+        this.handleLogIn = this.handleLogIn.bind(this);
+        this.handleForgotPassword = this.handleForgotPassword.bind(this);
+    }
+
+    handleEmailChange(event) {
+        this.setState({email: event.target.value}, null);
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.value}, null);
+    }
+
+    handleSignUp(event) {
+        alert('Signing Up with: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    handleLogIn(event) {
+        alert('Logging In with: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    handleForgotPassword(event) {
+        alert('Forgot Password with: ' + JSON.stringify(this.state));
+        event.preventDefault();
+    }
+
+    render() {
+
+        const emailInput = E('input', {
+            type: 'text',
+            value: this.state.email,
+            onChange: this.handleEmailChange,
+            placeholder: 'Email'
+        });
+
+        const passwordInput = E('input', {
+            type: 'password',
+            value: this.state.password,
+            onChange: this.handlePasswordChange,
+            placeholder: 'Password'
+        });
+
+        const signUpButton = E('button', {
+            type: 'button',
+            onClick: this.handleSignUp
+        }, 'Sign Up');
+
+        const logInButton = E('button', {
+            type: 'button',
+            onClick: this.handleLogIn
+        }, 'Log In');
+
+        const forgotButton = E('button', {
+            type: 'button',
+            onClick: this.handleForgotPassword
+        }, 'Forgot Password');
+
+        const buttons = E('div', null,
+            signUpButton,
+            logInButton,
+            forgotButton
+        );
+
+        return E('div', {className: 'AuthenticationForm'},
+            emailInput,
+            passwordInput,
+            buttons
+        );
+    }
+}
+
 
 ReactDOM.render(
-    React.createElement('h1', null, 'Hello World'),
+    E(AuthenticationPane, null),
     document.getElementById('root')
 );
